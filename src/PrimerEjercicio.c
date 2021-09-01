75894fe74ef53e08be9eeb4ffff23677069385f1
GtGt2021 <tecnimakinas@gmail.com>
2021-09-01 17:27:16 -0300

    primeros proyectos

---
+++ src/PrimerEjercicio.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : PrimerEjercicio.c
+ Author      : Gustavo Vargas
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Trabajo"); /* prints Trabajo */
+	return EXIT_SUCCESS;
+}
